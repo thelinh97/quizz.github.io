f54be020dabece027b45d2f1d375fabb4cc54405
Linh Vo <linhvo@Linhs-MacBook-Pro.local>
2023-05-04 16:53:47 +0700

    build

---
+++ build/precache-manifest.92def602afb5fa6807941199785e8a35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d10939e2f94d76744c03bc90f87f648",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3373e04cead09461472",
+    "url": "/static/js/2.747cb2b9.chunk.js"
+  },
+  {
+    "revision": "914a85462fc888825cb3a498c2b9869c",
+    "url": "/static/js/2.747cb2b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1571bf8c61c27ebbcc4c",
+    "url": "/static/js/main.452f27e1.chunk.js"
+  },
+  {
+    "revision": "5a5a4fbe531f50699c38",
+    "url": "/static/js/runtime-main.a961fa62.js"
+  },
+  {
+    "revision": "59eea93cd90531be861870538385728c",
+    "url": "/static/media/collov.59eea93c.jpg"
+  }
+]);
